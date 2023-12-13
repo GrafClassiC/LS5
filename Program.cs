44f32923aff626435fa5971f757c53c27daec63b
Graf <vlasov.art.ser@yandex.ru>
2023-12-13 17:38:42 +0400

    Lesson 5

--- Program.cs
+++ Program.cs
@@ -25,3 +25,87 @@
 */
 // Задача 2: Задайте двумерный массив. Напишите программу, 
 // которая поменяет местами первую и последнюю строку массива.
+/* internal class Program
+{
+    static void Main(string[] args)
+    {
+        int arr_n = int.Parse(Console.ReadLine());
+        int arr_m = int.Parse(Console.ReadLine());
+        int[,] arr = new int[arr_n, arr_m];
+        for(int i  = 0; i < arr_n; i++)
+        {
+            string[] numbers = Console.ReadLine().Split(' ');
+            for(int j = 0; j < arr_m; j++)
+            {
+                arr[i, j] = int.Parse(numbers[j]);
+            }
+        }
+        int[] intTemp1 = new int[arr_m];
+        int[] intTemp2 = new int[arr_m];
+        for (int i = 0; i < arr_m; i++)
+        {
+            intTemp1[i] = arr[0, i]; 
+            intTemp2[i] = arr[arr_n-1, i]; 
+        }
+        for (int j = 0; j < arr_m; j++)
+        {
+            arr[0, j] = intTemp2[j];
+            arr[arr_n - 1, j] = intTemp1[j];
+        }
+        for (int i = 0; i < arr_m; i++)
+        {
+            string res_ray = "";
+            for (int j = 0; j < arr_m; j++)
+            {
+                res_ray += arr[i, j] + " ";
+            }
+            Console.WriteLine(res_ray);
+        }
+        Console.ReadLine();
+    }
+}
+*/
+
+// Задача 3: Задайте прямоугольный двумерный массив. Напишите программу, 
+// которая будет находить строку с наименьшей суммой элементов.
+/* internal class Program
+{
+    static void Main(string[] args)
+    {
+        int arr_n = int.Parse(Console.ReadLine());
+        int arr_m = int.Parse(Console.ReadLine());
+        int[,] arr = new int[arr_n, arr_m];
+        for (int i = 0; i < arr_n; i++)
+        {
+            string[] numbers = Console.ReadLine().Split(' ');
+            for (int j = 0; j < arr_m; j++)
+            {
+                arr[i, j] = int.Parse(numbers[j]);
+            }
+        }
+        int minSum = 1;
+        int sum = SumLine(arr, 0);
+        for (int i = 1; i < arr.GetLength(0); i++)
+        {
+            if (sum > SumLine(arr, i))
+            {
+                sum = SumLine(arr, i);
+                minSum = i + 1;
+            }
+        }
+        Console.WriteLine($"\nСтрока c наименьшей суммой элементов: {minSum}");
+        Console.ReadLine();
+    }
+
+    static int SumLine(int[,] array, int i)
+    {
+        int sum = array[i, 0];
+        for (int j = 1; j < array.GetLength(1); j++)
+        {
+            sum += array[i, j];
+        }
+        return sum;
+    }
+}
+*/
+
